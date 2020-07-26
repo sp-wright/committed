2d6412f31b07f24493b710967e60f89ae66ed8e2
Stephen Wright <stephenwright@Stephens-MBP-2.broadband>
2020-07-26 17:05:04 +0100

    Updates

---
+++ precache-manifest.3f40597185a24d154983cb4c77e29b07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31dbd1eb5aab81f777c4a4774d05131",
+    "url": "/committed/index.html"
+  },
+  {
+    "revision": "a6a3cb482592bf1d7629",
+    "url": "/committed/static/css/main.3a9f95a6.chunk.css"
+  },
+  {
+    "revision": "1d97cd13871775cabd7f",
+    "url": "/committed/static/js/2.7bf54b71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/committed/static/js/2.7bf54b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a3cb482592bf1d7629",
+    "url": "/committed/static/js/main.a9f6035c.chunk.js"
+  },
+  {
+    "revision": "1095eadd0f9426ce13a8",
+    "url": "/committed/static/js/runtime-main.731544a4.js"
+  },
+  {
+    "revision": "c3693ee9ea7a17a7ba75c14b67ef8e10",
+    "url": "/committed/static/media/bg-img.c3693ee9.jpg"
+  },
+  {
+    "revision": "c1d24a1c253ec2919e4f09265d43d5d2",
+    "url": "/committed/static/media/bg-waves.c1d24a1c.svg"
+  },
+  {
+    "revision": "c1d24a1c253ec2919e4f09265d43d5d2",
+    "url": "/committed/static/media/bg-waves2.c1d24a1c.svg"
+  },
+  {
+    "revision": "d53cb2b4750ac76ad442da814a060faa",
+    "url": "/committed/static/media/main-img.d53cb2b4.svg"
+  }
+]);
